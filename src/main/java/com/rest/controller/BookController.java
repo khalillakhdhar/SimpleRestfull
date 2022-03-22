2b177923be985170e9af05902668ee59988031f6
khalil <khalillakhdharatc@gmail.com>
2022-03-22 16:16:00 +0100

    Update BookController.java

--- src/main/java/com/rest/controller/BookController.java
+++ src/main/java/com/rest/controller/BookController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.entities.Book;
 import com.rest.service.BookService;
 
 @RestController
+
 public class BookController {
 @Autowired
 BookService booksApi;
